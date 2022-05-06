52e86b5595e0559bddba2ab0b3e1ef0f06da7261
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-06 12:55:48 +0530

    fresh-commit

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,7 +1,9 @@
-resource "aws_security_group" "allow-ssh" {
+resource "aws_security_group" "allow_ssh" {
+
   vpc_id      = aws_vpc.main.id
-  name        = "allow-ssh"
+  name        = "allow_ssh"
   description = "security group that allows ssh and all egress traffic"
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -9,14 +11,18 @@ resource "aws_security_group" "allow-ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   tags = {
     Name = "allow-ssh"
   }
 }
 
+resource "aws_security_group_rule" "ingress_rules"{
+    count = length(var.rules)
+    type = "ingress"
+    from_port = var.rules[count.index].from_port
+    to_port = var.rules[count.index].to_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = var.rules[count.index].description
+    security_group_id =aws_security_group.allow_ssh.id
+}
--- var.tf
+++ var.tf
@@ -1,3 +1,28 @@
 variable "AWS_REGION" {
   default = "ap-southeast-1"
 }
+
+variable "rules" {
+    type=list(object({
+        from_port = number
+        to_port = number
+        protocol = string
+        cidr_block = string
+        description = string
+    }))
+
+    default = [{
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_block = "0.0.0.0/0"
+        description = "allow ssh connectivity"
+    },
+     {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_block = "0.0.0.0/0"
+        description = "allow web server connectivity"
+     }]
+}
